870b2c85758ddf76804f4959f929968d32699e22
Kartik Karkala <kartik.karkala@mail.mcgill.ca>
2019-02-12 04:22:32 -0500

    completed angular app and included all the required features

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import {HttpClient, HttpErrorResponse} from '@angular/common/http';
+
+interface Post {
+  userId: string;
+  id: string;
+  title: string;
+  body: string;
+}
 
 @Component({
   selector: 'app-root',
@@ -6,5 +14,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.less']
 })
 export class AppComponent {
-  title = 'angular-test';
+
+  title = 'Angular Reddit';
+  apiURL = 'https://jsonplaceholder.typicode.com/posts';
+  posts = [];
+  clicked = false;
+
+  constructor(private http: HttpClient) { }
+
+  getRandomNum(min, max): number {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+
+  getPosts(): void {
+    this.http.get<Post>(this.apiURL).subscribe(
+      data => this.posts.push(...data),
+      (err: HttpErrorResponse) => {
+        if (err.error instanceof Error) {
+          console.log('Client-side error occured');
+        } else {
+          console.log('Server-side error occured');
+        }
+      });
+  }
+
+  toggle(index): void {
+    this.posts[index].clicked = !this.posts[index].clicked;
+  }
 }
